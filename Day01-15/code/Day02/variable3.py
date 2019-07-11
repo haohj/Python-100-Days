9a9f08f590a29bc1c9af27b6872260adc79e721c
郝红剑 <hongjian.hao@zjtzsw.com>
2019-07-11 16:00:55 +0800

    运算符

---
+++ Day01-15/code/Day02/variable3.py
@@ -0,0 +1,17 @@
+"""
+格式化输出
+Version: 0.1
+Author: haohj
+Date: 2019-07-11
+"""
+
+a = int(input('a = '))
+b = int(input('b = '))
+
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
