18726e3b3a512dd5118974e1bbf75c1f34f5a32e
郝红剑 <hongjian.hao@zjtzsw.com>
2019-07-11 15:52:16 +0800

    运算符

---
+++ Day01-15/code/Day02/variable1.py
@@ -0,0 +1,23 @@
+"""
+使用变量保存数据并进行操作
+Version: 0.1
+Author: haohj
+Date: 2019-07-11
+"""
+a = 123
+b = 321
+
+# 加
+print(a + b)
+# 减
+print(a - b)
+# 乘
+print(a * b)
+# 除
+print(a / b)
+# 取整除 - 返回商的整数部分（向下取整）
+print(a // b)
+# 取模 - 返回除法的余数
+print(a % b)
+# 幂 - 返回x的y次幂
+print(a ** b)
---
+++ Day01-15/code/Day02/variable2.py
@@ -0,0 +1,9 @@
+"""
+将input函数输入的数据保存在变量中并进行操作
+Version: 0.1
+Author: haohj
+Date: 2019-07-11
+"""
+
+a = int(input('a = '))
+b = int(input('b = '))
