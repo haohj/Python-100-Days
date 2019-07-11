8735775135f1cc9158dbf7c3d2c42d6d7d87301c
郝红剑 <hongjian.hao@zjtzsw.com>
2019-07-11 16:02:48 +0800

    运算符

---
+++ Day01-15/code/Day02/variable4.py
@@ -0,0 +1,21 @@
+"""
+检查变量的类型
+Version: 0.1
+Author: haohj
+Date: 2019-07-11
+"""
+
+a = 100
+b = 1000000000000000000
+c = 12.345
+d = 1 + 5j
+e = 'A'
+f = 'hello, world'
+g = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
