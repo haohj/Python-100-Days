9d8eccd1f5bac808c7bd5e3389f2099e5f3457f0
郝红剑 <hongjian.hao@zjtzsw.com>
2019-07-11 16:51:53 +0800

    条件语句

---
+++ Day01-15/code/Day03/grade.py
@@ -0,0 +1,24 @@
+"""
+百分制成绩转等级制成绩
+90分以上，输出A
+80分~89分，输出B
+70分~79分，输出C
+60分~69分，输出D
+60分以下，输出E
+Version: 0.1
+Author: haohj
+Date: 2019-07-11
+"""
+
+score = float(input('请输入成绩:'))
+if score >= 90:
+    grade = 'A'
+elif score >= 80 and score < 89:
+    grade = 'B'
+elif score >= 70 and score < 79:
+    grade = 'C'
+elif score >= 60 and score < 69:
+    grade = 'D'
+else:
+    grade = 'E'
+print('对应的等级是:', grade)
