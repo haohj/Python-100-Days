57b5d20a2cf5f787fb2bf66fc27dcc8a437b60b5
郝红剑 <hongjian.hao@zjtzsw.com>
2019-07-11 17:11:38 +0800

    条件语句

---
+++ Day01-15/code/Day03/piecewise.py
@@ -0,0 +1,18 @@
+"""
+分段函数求值
+        3x - 5  (x > 1)
+f(x) =  x + 2   (-1 <= x <= 1)
+        5x + 3  (x < -1)
+Version: 0.1
+Author: haohj
+Date: 2019-07-11
+"""
+
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+elif x >= -1:
+    y = x + 2
+else:
+    y = 5 * x + 3
+print('f(%.2f) = %.2f' % (x, y))
